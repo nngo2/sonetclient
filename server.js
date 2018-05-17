085136c2142d2a0c7a04df2445e3c9c426120e6c
namngo <hnnngo@mum.edu>
2018-05-16 19:44:02 -0500

    Fix server path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/SonetClient'));
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/SonetClient/index.html'));
